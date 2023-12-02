3040e5b5b2b7e6831fad23a5f284c6744083f821
taniadavy <tania111davidenko111@gmail.com>
2023-12-02 02:19:44 +0100

    loadmore button

--- src/js/index.js
+++ src/js/index.js
@@ -11,28 +11,38 @@ const refs = {
     loadMoreBtn: document.querySelector(".js-load-more")
 }
 
+let page = 1;
+const input = refs.form.elements[0];
+
 
 refs.form.addEventListener("submit", onSubmit);
 async function onSubmit(event){
     event.preventDefault();
 
-    const input = refs.form.elements[0];
     const inputData = input.value.trim();
 
     try {
-        const searchData = await serviceSearch(inputData);
+        const searchData = await serviceSearch(inputData, page);
         console.log(searchData);
         const array= searchData.hits
+        const totalImg = searchData.total
 
         if(searchData.totalHits === 0){
             Notiflix.Notify.failure('Sorry, there are no images matching your search query. Please try again.');
         } else {
-            const totalImg = searchData.total
             Notiflix.Notify.success(`Hooray! We found ${totalImg} images`);
         }
 
         console.log(array);
         refs.gallery.innerHTML = createMarkup(array);
+
+        totalpages = totalImg / 40;
+        console.log(totalpages)
+
+        if(page >= 1 && page < totalpages) {
+            refs.loadMoreBtn.classList.remove("hidden");
+            refs.loadMoreBtn.addEventListener("click", handleMoreBtn);
+        }
 
     }
     catch (err){
@@ -41,11 +51,32 @@ async function onSubmit(event){
 
 }
 
+async function handleMoreBtn() {
+    page += 1;
+    refs.loadMoreBtn.disabled = true;
 
-async function serviceSearch(requestData){
-    const BASE_URL = "https://pixabay.com/api";
-    const API_KEY = "41011803-a96263b547d952e0549f5a687"
+    const data = input.value
+    console.log(data)
+    const newSearch = await serviceSearch(data, page);
+    console.log(newSearch);
+    const nextArray= newSearch.hits
 
-    const {data} = await axios.get(`${BASE_URL}/?key=${API_KEY}&per_page=40&q=${requestData}`);
-    return data;    
+    refs.gallery.insertAdjacentHTML("beforeend", createMarkup(nextArray));
+    refs.loadMoreBtn.disabled = false;
 };
+
+
+async function serviceSearch(requestData, page){
+    try{
+        const BASE_URL = "https://pixabay.com/api";
+        const API_KEY = "41011803-a96263b547d952e0549f5a687"
+        const {data} = await axios.get(`${BASE_URL}/?key=${API_KEY}&per_page=40&page=${page}&q=${requestData}`);
+        return data;
+    } catch(err) {
+        console.error(err);
+    }     
+};
+
+
+console.log(page)
+
